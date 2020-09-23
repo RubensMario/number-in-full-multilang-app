c40ae0108342a9fd8488e2c801b683a3015752de
RubensMario <newrmmcf@gmail.com>
2020-09-23 19:09:06 -0300

    Style updates

--- README.md
+++ README.md
@@ -5,12 +5,16 @@ An app that uses the number-in-full-multilang lib to convert into words a value 
 ## Installing
 
 - Clone this repository
-
-\$ git clone https://github.com/RubensMario/number-in-full-multilang-app.git
+  
+  ```
+  \$ git clone https://github.com/RubensMario/number-in-full-multilang-app.git
+  ```
 
 - Install the number-in-full-multilang lib
-
-npm install number-in-full-multilang
+  
+  ```
+  npm install number-in-full-multilang
+  ```
 
 - Open the index.html file on your browser
 
@@ -23,3 +27,5 @@ npm install number-in-full-multilang
 ## Author
 
 Rubens Mario
+
+- 
--- server.js
+++ server.js
@@ -3,8 +3,20 @@ import express from 'express';
 import * as path from 'path';
 
 const app = express();
+// process.env.PORT: variável requerida pelo Heroku para o build
+// com valor preenchido pelo seu servidor para determinar porta a ser usada
 const port = process.env.PORT || 3338;
+// path.resolve() dá o caminho absoluto a partir de um caminho relativo
+// path.resolve(arquivo.txt) -> /User/Documentos/arquivo.txt
 const __dirname = path.resolve();
+
+/*Ao acessarem o diretório raiz '/', 
+será servida a página estática em /.../public
+através da porta definida em port
+ */
+
+// path.join concatena os dois argumentos de entrada
+// path.join(/a,/b) -> /a/b
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
